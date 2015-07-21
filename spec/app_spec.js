cc05b5d8745ad5fc6613f1393d70a4f27d38aa7e
changecase <jeff.ja@gmail.com>
2015-07-20 22:17:34 -0700

    New features: #retrieve AJAX, #parse, #hideBanner
    The .retrieve() method currently logs to the console in order to spy on
    the response it grabs. This should get replaced with something else as
    soon as possible.
    
    This method will return a JSON object when (if) the AJAX request was
    successful.
    
    The .parse() method takes a callback function as an argument. When
    combined with the .retrieve() method, you can parse the JSON returned by
    .retrieve(). It returns an object with the urls for any *.mp4, *.ogv, or
    *.webm videos that have been uploaded and associated with this
    Squarespace page.
    
    The .hideBanner() method has been initiated. We'll end up comparing this
    with each of the default templates (just for coverage).

--- spec/app_spec.js
+++ spec/app_spec.js
@@ -1,21 +1,98 @@
-import { jQuery, $ } from "../node_modules/jquery/dist/jquery";
+import $ from "../node_modules/jquery/dist/jquery";
 import BannerVideo from '../src/app.js';
 
 describe("Banner Video", function () {
-  let bannerVideo, jsonResponse;
-  jasmine.getJSONFixtures().fixturesPath = 'base/spec/javascripts/fixtures/json/';
+  let bannerVideo, jsonResponse, jsonResponseNoVid, fixture;
+  jasmine.getJSONFixtures().fixturesPath  = 'base/spec/javascripts/fixtures/json/';
 
-  beforeEach(function(){
+  beforeEach(function() {
     bannerVideo = new BannerVideo();
+
     jsonResponse = getJSONFixture('sqsPageData.json');
-    jasmine.Ajax.install();
+    jsonResponseNoVid = getJSONFixture('sqsPageDataNoVid.json');
+
+    jasmine.Ajax.stubRequest('/test?format=json').andReturn({
+      "status": 200,
+      "contentType": 'json',
+      "responseText": jsonResponse
+    });
+
+    jasmine.Ajax.stubRequest('/does/not/exist?format=json').andReturn({
+      "status": 400
+    });
+
+    jasmine.Ajax.stubRequest('/not/video/enabled').andReturn({
+      "status": 200,
+      "contentType": 'json',
+      "responseText": jsonResponseNoVid
+    });
   });
 
   describe("#retrieve", function () {
+    beforeEach(function(){
+      jasmine.Ajax.install();
+    });
+
+    afterEach(function(){
+      jasmine.Ajax.uninstall();
+    });
+
     it("should be a method of the BannerVideo object", function () {
       expect( bannerVideo.retrieve() ).toBeDefined;
     });
 
+    describe("AJAX request", function () {
+      var consoleSpy, result;
+
+      it("should log text from the service to the console",function () {
+        consoleSpy = spyOn(console, "log");
+        result = bannerVideo.retrieve('/test');
+        expect(consoleSpy).toHaveBeenCalledWith(jsonResponse);
+      });
+
+      it("should return an object if the customType is 'video-enabled-page'", function() {
+        expect(bannerVideo.retrieve('/test')).toBeTruthy();
+      });
+
+      it("should NOT return an object if the customType is 'video-enabled-page'", function () {
+        expect(bannerVideo.retrieve('/not/video/enabled')).toBe(false);
+        expect(bannerVideo.retrieve('/does/not/exist')).toBe(false);
+      });
+    });
+  });
+
+  describe("#parse", function () {
+    beforeEach(function(){
+      jasmine.Ajax.install();
+    });
+
+    afterEach(function(){
+      jasmine.Ajax.uninstall();
+    });
+
+    it("should return an object with mp4, ogv, and webm properties", function () {
+        var json = bannerVideo.retrieve('/test'),
+            result = bannerVideo.parse(json);
+
+        expect(result.mp4).toBeDefined();
+        expect(result.ogv).toBeDefined();
+        expect(result.webm).toBeDefined();
+    });
+  });
+
+  describe("#hideBanner", function () {
+    let fixture;
+
+    beforeEach(function () {
+      jasmine.getFixtures().fixturesPath = 'base/spec/javascripts/fixtures';
+    });
+
+    it("should hide the banner image on the Five template", function () {
+      fixture = loadFixtures('sqs-demo-five.html');
+
+      bannerVideo.hideBanner();
+      expect( $('#page-thumb img') ).toBeHidden();
+    });
   });
 });
 
--- src/app.js
+++ src/app.js
@@ -1,7 +1,54 @@
-import {jQuery, $} from "../node_modules/jquery/dist/jquery";
+import $ from "../node_modules/jquery/dist/jquery";
 
 class BannerVideo {
-  retrieve() {
+  retrieve(uri) {
+    let xhr = new XMLHttpRequest(),
+        rsp;
+
+    xhr.onreadystatechange = function (args) {
+      if (this.readyState == this.DONE) {
+        console.log(this.responseText);
+        rsp = this.responseText;
+      }
+    };
+
+    xhr.open("GET", uri + '?format=json');
+    xhr.send();
+
+    if (xhr.status == 200) {
+      return rsp;
+    } else {
+      return false;
+    }
+  }
+
+  parse(f) {
+    let rspObj = {};
+
+    if ( f.hasOwnProperty('items') && 
+         f.items[0].hasOwnProperty('customContent') &&
+         f.items[0].customContent.hasOwnProperty('customType') &&
+         f.items[0].customContent.customType == 'video-enabled-page' 
+       ) {
+      rspObj.mp4  = f.items[0].customContent.mp4Source;
+      rspObj.ogv  = f.items[0].customContent.ogvSource;
+      rspObj.webm = f.items[0].customContent.webmSource;
+    }
+
+    return rspObj;
+  }
+
+  hideBanner() {
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      return;
+    } else {
+      let banner = '';
+
+      console.log(banner.length);
+      if (banner.length === 0) { banner = $('#page-thumb img'); }
+
+      banner.hide();
+    }
   }
 };
 
